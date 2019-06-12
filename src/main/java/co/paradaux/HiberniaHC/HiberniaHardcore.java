9b8257723d177fa60e5bc4b5fa4d616eb7d15103
Rían Errity <rian@paradaux.ie>
2019-06-12 15:16:17 +0100

    Initial commit

---
+++ src/main/java/co/paradaux/HiberniaHC/HiberniaHardcore.java
@@ -0,0 +1,13 @@
+package co.paradaux.HiberniaHC;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HiberniaHardcore extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+
+		getLogger().info("Hello World!");
+
+	}
+}
