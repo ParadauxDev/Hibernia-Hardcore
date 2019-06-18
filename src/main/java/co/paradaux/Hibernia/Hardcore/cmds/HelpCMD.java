0cbd7d7e9878325c4ff6cd8136a71cfee57319aa
CommanderCortex <cjcotton88@gmail.com>
2019-06-19 09:36:23 +1200

    HELP ME PARA :cry:

--- src/main/java/co/paradaux/Hibernia/Hardcore/cmds/HelpCMD.java
+++ src/main/java/co/paradaux/Hibernia/Hardcore/cmds/HelpCMD.java
@@ -1,5 +1,20 @@
 package co.paradaux.Hibernia.Hardcore.cmds;
 
-public class HelpCMD {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class HelpCMD implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if(!(sender instanceof Player)) {
+			System.out.println("Why Can I Not Use Bukkit in here? HELP ME PARA");
+		}
+		
+		
+		return false;
+	}
 
 }
